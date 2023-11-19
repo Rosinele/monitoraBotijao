650db3d9ac767c1b9665338c6603b21572f7780f
rosinele <rosefelix99@gmail.com>
2023-11-19 19:29:50 -0300

    removendo graficos desnecessarios

--- src/pages/admin/dashboard/Dashboard.vue
+++ src/pages/admin/dashboard/Dashboard.vue
@@ -1,26 +1,5 @@
 <template>
-  <div class="dashboard">
-    <dashboard-charts />
-
-    <dashboard-info-block />
-
-    <div class="grid grid-cols-12 gap-6">
-      <dashboard-tabs class="col-span-12 lg:col-span-6" @submit="addAddressToMap" />
-
-      <dashboard-map ref="dashboardMap" class="col-span-12 lg:col-span-6" />
-    </div>
-    <div class="flex justify-center mt-2">
-      <va-button
-        class="mr-2"
-        size="small"
-        preset="plain"
-        href="https://www.iubenda.com/privacy-policy/19055770"
-        title="Privacy Policy"
-      >
-        Privacy Policy
-      </va-button>
-    </div>
-  </div>
+  
 </template>
 
 <script setup lang="ts">
